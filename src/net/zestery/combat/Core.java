c5388b2a04e1b5da3d4f51d77160134f8d940988
ZeSteRy <alexandre@1s.fr>
2016-03-01 17:08:48 +0100

    Add base

--- src/net/zestery/combat/Core.java
+++ src/net/zestery/combat/Core.java
@@ -4,6 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
 
+    @Override
+    public void onLoad(){
+
+    }
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
 
 
 }
